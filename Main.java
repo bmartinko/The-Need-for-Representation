28e93a71e3dbd235afdfccabb6e53c8e9e161cb7
bmartinko <bennett.martinko@gmail.com>
2018-09-24 10:48:08 -0400

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner kbsReader = new Scanner(System.in);
+        System.out.println("Please write a word");
+        String word = kbsReader.nextLine();
+
+        int wordLength = word.length();
+        int randomNumber = Math.random(1,5)
+    }
+}
